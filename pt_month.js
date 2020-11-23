b479494543d71c7c8d4b6ae1086b043bcb723b8b
learnICT <kibrisli7@gmail.com>
2020-11-23 12:32:53 +0000

    included jamaah times into monthly script

--- pt_month.js
+++ pt_month.js
@@ -40,12 +40,12 @@
     var bugun = weekday[today.getDay()];
     //console.log(month);
     mytable = '<table class="table"><thead class=" thead-light"><tr><th scope="col">'+ month +'</th>';
-    mytable += '<th scope="col">Fajr <img src="images/sunrise.png" width="15px" alt="Fajr"></th>';
+    mytable += '<th scope="col" colspan="2">Fajr <img src="images/sunrise.png" width="15px" alt="Fajr"></th>';
     mytable += '<th scope="col">Sunrise</th>';
-    mytable += '<th scope="col">Dhuhr <img src="images/sun.png" width="15px" alt="Dhuhr"></th>';
-    mytable += '<th scope="col">Asr <img src="images/sunset.png" width="15px" alt="Asr"></th>';
-    mytable += '<th scope="col">Maghrib <img src="images/moonrise.png" width="15px" alt="Maghrib"></th>';
-    mytable += '<th scope="col">Isha <img src="images/night.png" width="15px" alt="Isha"></th></tr></thead><tbody>';
+    mytable += '<th scope="col" colspan="2">Dhuhr <img src="images/sun.png" width="15px" alt="Dhuhr"></th>';
+    mytable += '<th scope="col" colspan="2">Asr <img src="images/sunset.png" width="15px" alt="Asr"></th>';
+    mytable += '<th scope="col" colspan="2">Maghrib <img src="images/moonrise.png" width="15px" alt="Maghrib"></th>';
+    mytable += '<th scope="col" colspan="2">Isha <img src="images/night.png" width="15px" alt="Isha"></th></tr></thead><tbody>';
     //console.log(pts)
     let newday = '';
     
@@ -59,18 +59,23 @@
       var obj = pts[gun];
       //console.log(obj);
       var fajr = obj['fajr'];
+      var fajr_j = obj['fajr_jamat'];
       var sun = obj['sunrise'];
       var dhu = obj['dhuhr'];
+      var dhu_j = obj['dhuhr_jamat'];
       var asr = obj['asr'];
+      var asr_j = obj['asr_jamat'];
       var mag = obj['magrib'];
+      var mag_j = obj['magrib_jamat'];
       var ish = obj['isha'];
+      var ish_j = obj['isha_jamat'];
       //console.log(today_date)
       if (obj['date']==today_date){
-        newday = "<tr class='table-active'><td>"+(i+1)+"</td><td>"+fajr+"</td><td>"+sun+"</td><td>"+dhu+"</td><td>"+asr+"</td><td>"+mag+"</td><td>"+ish+"</td></tr>";
+        newday = "<tr class='table-active'><td>"+(i+1)+"</td><td>"+fajr+"</td><td>"+fajr_j+"</td><td>"+sun+"</td><td>"+dhu+"</td><td>"+dhu_j+"</td><td>"+asr+"</td><td>"+asr_j+"</td><td>"+mag+"</td><td>"+mag_j+"</td><td>"+ish+"</td><td>"+ish_j+"</td></tr>";
       }else if (weekday[today.getDay()]=='Friday') {
-        newday = "<tr class='table-warning'><td>"+(i+1)+"</td><td>"+fajr+"</td><td>"+sun+"</td><td>"+dhu+"</td><td>"+asr+"</td><td>"+mag+"</td><td>"+ish+"</td></tr>";
+        newday = "<tr class='table-warning'><td>"+(i+1)+"</td><td>"+fajr+"</td><td>"+fajr_j+"</td><td>"+sun+"</td><td>"+dhu+"</td><td>"+dhu_j+"</td><td>"+asr+"</td><td>"+asr_j+"</td><td>"+mag+"</td><td>"+mag_j+"</td><td>"+ish+"</td><td>"+ish_j+"</td></tr>";
       }else{
-      newday = "<tr><td>"+(i+1)+"</td><td>"+fajr+"</td><td>"+sun+"</td><td>"+dhu+"</td><td>"+asr+"</td><td>"+mag+"</td><td>"+ish+"</td></tr>";
+      newday = "<tr><td>"+(i+1)+"</td><td>"+fajr+"</td><td>"+fajr_j+"</td><td>"+sun+"</td><td>"+dhu+"</td><td>"+dhu_j+"</td><td>"+asr+"</td><td>"+asr_j+"</td><td>"+mag+"</td><td>"+mag_j+"</td><td>"+ish+"</td><td>"+ish_j+"</td></tr>";
       }
       mytable += newday;
 
